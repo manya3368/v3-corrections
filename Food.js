7e182dc12289454cc1e4d8bdf774cb19ed87c069
manya3368 <71918637+manya3368@users.noreply.github.com>
2020-12-10 22:39:23 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,78 @@
+class Food {
+  constructor() {
+    this.foodStock = 0;
+    this.lastFed;
+    this.image = loadImage("Milk.png");
+  }
+
+  updateFoodStock(foodStock) {
+    this.foodStock = foodStock;
+  }
+
+  getFedTime(lastFed) {
+    this.lastFed = lastFed;
+  }
+
+  deductFood() {
+    if (this.foodStock > 0) {
+      this.foodStock = this.foodStock - 1;
+    }
+  }
+
+  getFoodStock() {
+    return this.foodStock;
+  }
+
+  display() {
+    var x = 80, y = 100;
+
+    imageMode(CENTER);
+    image(this.image, 720, 220, 70, 70);
+
+    if (this.foodStock != 0) {
+      for (var i = 0; i < this.foodStock; i++) {
+        if (i % 10 == 0) {
+          x = 80;
+          y = y + 50;
+        }
+        image(this.image, x, y, 50, 50);
+        x = x + 30;
+      }
+    }
+  }
+
+  bedroom() {
+    background(bedroomimg, 500, 550);
+  }
+
+  garden() {
+    background(gardenimg, 500, 550);
+  }
+
+  washroom() {
+    background(washroomimg, 500, 550);
+  }
+}
+  currentTime = hour();
+
+  if(currentTime === (lastFed + 1)) {
+  update('Playing');
+  foodObj.garden();
+
+}
+
+   else if (currentTime === (lastFed + 2)) {
+  update("sleeping");
+  foodObj.bedroom();
+}
+else if (currentTime > (lastFed + 2)) {
+  update("Bathing");
+  foodObj.washroom();
+}
+ else{
+   update("Hungry")
+   foodObj.display();
+ }
+
+
+
